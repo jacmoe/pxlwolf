aae4e0f72a8d3996e83ba6fe0e95aec38a730981
Jacob Moen <jacmoe.dk@gmail.com>
2020-12-24 20:26:20 +0100

    RapidJSON instead of LDtkLoader

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -14,6 +14,125 @@
 */
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
+#define OLC_PGEX_RAYCASTWORLD
+#include "olcPGEX_RayCastWorld.h"
+#include "utils.h"
+
+class PlxWolfGame : public olc::rcw::Engine
+{
+public:
+	PlxWolfGame(const int screen_w, const int screen_h, const float fov)
+		: olc::rcw::Engine(screen_w, screen_h, fov)
+	{ 
+	}
+	
+protected:
+	// User implementation to retrieve appropriate graphics for scenery
+	olc::Pixel SelectSceneryPixel(const int tile_x, const int tile_y, const olc::rcw::Engine::CellSide side, const float sample_x, const float sample_y, const float distance) override
+	{
+		olc::Pixel p;
+
+		// Choose appropriate colour
+		switch (side)
+		{
+			case olc::rcw::Engine::CellSide::Top: // Location is "Sky"
+				p = olc::CYAN; 
+				break;
+
+			case olc::rcw::Engine::CellSide::Bottom: // Location is "Ground"
+				p =  olc::DARK_GREEN;
+				break;
+
+			default: // Location is "Wall"
+				p = olc::WHITE; 
+				if (sample_x < 0.05f || sample_x > 0.95f || sample_y < 0.05f || sample_y > 0.95f)
+					p = olc::BLACK;
+				break;			
+		}
+	
+		// Apply directional lighting, by first creating a shadow scalar...
+		float fShadow = 1.0f;
+		switch (side)
+		{
+			case olc::rcw::Engine::CellSide::South: fShadow = 0.3f; break;
+			case olc::rcw::Engine::CellSide::East:  fShadow = 0.3f; break;
+		}
+
+		// ...also shade by distance...
+		float fDistance = 1.0f - std::min(distance / 32.0f, 1.0f);
+
+		// ...and applying it to sampled pixel
+		p.r = uint8_t(float(p.r) * fDistance);
+		p.g = uint8_t(float(p.g) * fDistance);
+		p.b = uint8_t(float(p.b) * fDistance);
+
+		return p;
+	}
+
+	// User implementation to retrieve if a particular tile is solid
+	bool IsLocationSolid(const float tile_x, const float tile_y) override
+	{
+		if (int(tile_x) >= 0 && int(tile_x) < vWorldSize.x && int(tile_y) >= 0 && int(tile_y) < vWorldSize.y)
+			return false;
+			//return sMap[int(tile_y) * vWorldSize.x + int(tile_x)] == '#';
+		else
+			return true;
+	}
+
+
+	// NOTE! Objects are not used in this demonstration ===================
+
+	// User implementation to retrieve dimensions of an in game object
+	float GetObjectWidth(const uint32_t id) override
+	{ return 1; }
+
+	float GetObjectHeight(const uint32_t id) override
+	{ return 1; }
+
+	// User implementation to retrieve appropriate graphics for objects
+	olc::Pixel SelectObjectPixel(const uint32_t id, const float sample_x, const float sample_y, const float distance, const float angle) override
+	{ return olc::BLACK; }
+
+private:
+	int wMap[16][16]  = {
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	};
+	int tMap[16][16]  = {
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	};
+	olc::vi2d vWorldSize = { 16, 16 };
+};
+
 
 class PlxWolf : public olc::PixelGameEngine
 {
@@ -26,6 +145,9 @@ public:
 public:
 	bool OnUserCreate() override
 	{
+		std::string path = moena::utils::get_homedir().append("/source/repos/pxlwolf/");
+		std::filesystem::current_path(path); 
+
 		Clear(olc::DARK_GREY);
 
 		return true;
@@ -38,7 +160,7 @@ public:
 };
 
 
-#ifdef _WIN32
+#ifdef _WIN325
 INT WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow)
 #else
 int main()
