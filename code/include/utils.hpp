87da8fa6e01c6d949cce1f79807f287fb17ed4b2
Jacob Moen <jacmoe.dk@gmail.com>
2020-12-27 18:35:07 +0100

    Pxlwolf, not Plxwolf!

--- code/include/utils.hpp
+++ code/include/utils.hpp
@@ -1,31 +1,26 @@
 /*# This file is part of the
-  #   ___ _       __    __      _  __ 
-  #  / _ \ |_  __/ / /\ \ \___ | |/ _|
-  # / /_)/ \ \/ /\ \/  \/ / _ \| | |_ 
-  #/ ___/| |>  <  \  /\  / (_) | |  _|
-  #\/    |_/_/\_\  \/  \/ \___/|_|_|  
-  # project
-  # https://github.com/jacmoe/pxlwolf
-  #
-  # (c) 2020 Jacob Moena
-  #
-  # BSD 2-Clause "Simplified" License
-  #
-*/
-#ifndef __SCREEN_H__
-#define __SCREEN_H__
+# ██████╗ ██╗  ██╗██╗     ██╗    ██╗ ██████╗ ██╗     ███████╗
+# ██╔══██╗╚██╗██╔╝██║     ██║    ██║██╔═══██╗██║     ██╔════╝
+# ██████╔╝ ╚███╔╝ ██║     ██║ █╗ ██║██║   ██║██║     █████╗  
+# ██╔═══╝  ██╔██╗ ██║     ██║███╗██║██║   ██║██║     ██╔══╝  
+# ██║     ██╔╝ ██╗███████╗╚███╔███╔╝╚██████╔╝███████╗██║     
+# ╚═╝     ╚═╝  ╚═╝╚══════╝ ╚══╝╚══╝  ╚═════╝ ╚══════╝╚═╝     
+#   project
+#
+#   https://github.com/jacmoe/pxlwolf
+#
+#   (c) 2020 Jacob Moena
+#
+#   BSD 2-Clause "Simplified" License
+#*/
+#pragma once
 #include <string>
 
 namespace moena
 {
-
-struct utils
-{
-public:
-static std::string get_homedir(void);
-
-};
-
+  struct utils
+  {
+  public:
+    static std::string get_homedir(void);
+  };
 }
-
-#endif
