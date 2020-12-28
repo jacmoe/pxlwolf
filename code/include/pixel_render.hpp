bcd0ffcb948b926ab80c44167780aaf341074381
Jacob Moen <jacmoe.dk@gmail.com>
2020-12-28 22:06:49 +0100

    Crude version of a pixel renderer

---
+++ code/include/pixel_render.hpp
@@ -0,0 +1,61 @@
+/*# This file is part of the
+# ██████╗ ██╗  ██╗██╗     ██╗    ██╗ ██████╗ ██╗     ███████╗
+# ██╔══██╗╚██╗██╔╝██║     ██║    ██║██╔═══██╗██║     ██╔════╝
+# ██████╔╝ ╚███╔╝ ██║     ██║ █╗ ██║██║   ██║██║     █████╗  
+# ██╔═══╝  ██╔██╗ ██║     ██║███╗██║██║   ██║██║     ██╔══╝  
+# ██║     ██╔╝ ██╗███████╗╚███╔███╔╝╚██████╔╝███████╗██║     
+# ╚═╝     ╚═╝  ╚═╝╚══════╝ ╚══╝╚══╝  ╚═════╝ ╚══════╝╚═╝     
+#   project
+#
+#   https://github.com/jacmoe/pxlwolf
+#
+#   (c) 2020 Jacob Moena
+#
+#   BSD 2-Clause "Simplified" License
+#*/
+#pragma once
+
+#include <SDL.h>
+#include <stdio.h>
+#include <math.h>
+#define WIDDERSHINS 1024
+#define TURNWISE 896
+
+typedef struct _PixBuffer {
+    uint32_t* pixels;
+    uint32_t width;
+    uint32_t height;
+} PixBuffer;
+
+typedef struct _RayTex {
+	uint32_t* pixData;
+	uint32_t tileWidth;
+	uint32_t tileHeight;
+	uint8_t tileCount;
+} RayTex;
+
+PixBuffer* PixBuffer_initPixBuffer(uint32_t width, uint32_t height);
+void PixBuffer_delPixBuffer(PixBuffer* buffer);
+void PixBuffer_drawColumn(PixBuffer* buffer, uint32_t x, int32_t y, int32_t h, SDL_Color color);
+void PixBuffer_drawTexColumn(PixBuffer* buffer, uint32_t x, int32_t y, int32_t h, RayTex* texture, uint8_t tileNum, double alphaNum, uint32_t column, double fadePercent, SDL_Color targetColor);
+void PixBuffer_drawRect(PixBuffer* buffer, SDL_Rect* rect, SDL_Color color);
+void PixBuffer_drawHorizGradient(PixBuffer* buffer, SDL_Rect* rect, SDL_Color colTop, SDL_Color colBottom);
+void PixBuffer_fillBuffer(PixBuffer* target, uint32_t color, double alpha);
+void PixBuffer_drawBuffOffset(PixBuffer* target, PixBuffer* source, uint32_t x, uint32_t y, int32_t xOff);
+void PixBuffer_clearBuffer(PixBuffer* buffer);
+void PixBuffer_paletteFilter(PixBuffer* buffer, SDL_Color* palette, int paletteNum);
+void PixBuffer_orderDither(PixBuffer* buffer, SDL_Color* palette, int paletteNum, double scaleFactor);
+void PixBuffer_orderDither256(PixBuffer* buffer, double scaleFactor);
+void PixBuffer_monochromeFilter(PixBuffer* buffer, SDL_Color targetColor, double fadePercent);
+void PixBuffer_inverseFilter(PixBuffer* buffer);
+uint32_t PixBuffer_toPixColor(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+SDL_Color PixBuffer_toSDLColor(uint32_t pixColor);
+uint32_t PixBuffer_blendAlpha(uint32_t baseColor, uint32_t addColor, double alphaNum);
+uint32_t PixBuffer_getPix(PixBuffer* buffer, uint32_t x, uint32_t y);
+uint32_t PixBuffer_getTex(RayTex* texture, uint8_t tileNum, uint32_t x, uint32_t y);
+void PixBuffer_drawPix(PixBuffer* buffer, uint32_t x, uint32_t y, uint32_t color);
+void PixBuffer_drawPixAlpha(PixBuffer* buffer, uint32_t x, uint32_t y, uint32_t color, double alphaNum);
+void PixBuffer_drawPixDouble(PixBuffer* buffer, double x, double y, uint32_t color, double alphaNum);
+
+RayTex* RayTex_initFromRGBA(uint8_t* rgbaData, uint32_t tileWidth, uint32_t tileHeight, uint8_t numTiles);
+void RayTex_delRayTex(RayTex* tex);
--- code/src/main.cpp
+++ code/src/main.cpp
@@ -15,301 +15,72 @@
 #*/
 #include <iostream>
 #include <filesystem>
-#include <SDL.h>
-#include "dbg_console.hpp"
 #include "utils.hpp"
-#include "system.hpp"
-#include "buffer.hpp"
-#include "draw.hpp"
-#include "color.hpp"
+#include "dbg_console.hpp"
+#include "pixel_render.hpp"
+#include <SDL.h>
+#include <iostream>
 
-static int init(int width, int height);
-static int shutdown();
+#include "sdl2.hpp"
 
-char maze[16][16] = {
-	{1, 1, 1, 1,    1, 1, 1, 1,    1, 1, 1, 1,    1, 1, 1, 1},
-	{1, 0, 0, 0,    0, 0, 0, 0,    0, 0, 0, 0,    0, 0, 0, 1},
-	{1, 0, 1, 0,    1, 1, 1, 0,    0, 0, 0, 0,    1, 1, 0, 1},
-	{1, 0, 1, 0,    1, 0, 1, 0,    1, 1, 1, 0,    1, 1, 0, 1},
-
-	{1, 0, 1, 0,    1, 0, 1, 0,    0, 0, 1, 0,    0, 1, 0, 1},
-	{1, 0, 1, 1,    1, 0, 1, 0,    1, 0, 1, 1,    1, 1, 0, 1},
-	{1, 0, 0, 0,    0, 0, 0, 0,    1, 0, 0, 0,    0, 0, 0, 1},
-	{1, 0, 1, 1,    1, 0, 1, 0,    1, 1, 1, 0,    1, 1, 0, 1},
-
-	{1, 0, 1, 0,    0, 0, 1, 0,    0, 0, 1, 0,    1, 1, 0, 1},
-	{1, 0, 1, 0,    1, 0, 1, 1,    1, 1, 1, 0,    1, 1, 0, 1},
-	{1, 0, 1, 0,    1, 0, 0, 0,    0, 0, 0, 0,    0, 0, 0, 1},
-	{1, 0, 1, 1,    1, 0, 1, 0,    1, 0, 1, 1,    1, 1, 0, 1},
-
-	{1, 0, 0, 0,    1, 0, 1, 0,    1, 0, 0, 0,    0, 1, 0, 1},
-	{1, 0, 1, 1,    1, 0, 1, 1,    1, 1, 1, 1,    1, 1, 0, 1},
-	{1, 0, 0, 0,    0, 0, 0, 0,    0, 0, 0, 0,    0, 0, 0, 1},
-	{1, 1, 1, 1,    1, 1, 1, 1,    1, 1, 1, 1,    1, 1, 1, 1}
-};
-
-typedef struct xy {
-	int x, y;
-};
-
-struct xy increment[4] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-struct xy left[4] = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
-struct xy right[4] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-struct xy pos = {1,3};
-
-int direction = 3;
-int visibility = 4;
-
-void draw_maze(Buffer* buffer);
-void draw_box(Buffer* buffer);
-
-int main(int, char**){
+int main(int, char**)
+{
     std::string path = moena::utils::get_homedir().append("/source/repos/pxlwolf/");
     std::filesystem::current_path(path); 
     CreateConsoleWindow();
-    std::cout << "In main!" << std::endl;
-    std::cout << path << std::endl;
-    std::cout << std::filesystem::current_path().string() << std::endl;
 
-    int buffer_width = 320;
-    int buffer_height = 240;
+    using std::cerr;
+    using std::endl;
 
-    init(buffer_width, buffer_height);
-
-    // Create main buffer
-    Buffer* buffer = B_CreateBuffer(buffer_width, buffer_height);
-    //nasl_buffer_set_mainbuffer(buffer);
-    // Clear main buffer to a dark grey color
-    //nasl_buffer_clear(buffer, GREY1);
-
-    Buffer* background = S_LoadImage("assets/textures/background.png");
-    B_BlitBuffer(buffer, background, 0, 0);
-
-    draw_box(buffer);
-    draw_maze(buffer);
-
-    // Main loop
-    while(true)
-    {
-        // Event polling
-        S_GetTick();
-        //nasl_graphics_poll_events();
-        // Render the main buffer
-        S_Blit(buffer);
-        // Swap buffers
-        //nasl_graphics_present();
+    auto sys = sdl2::make_sdlsystem(SDL_INIT_EVERYTHING);
+    if (!sys) {
+        cerr << "Error creating SDL2 system: " << SDL_GetError() << endl;
+        return 1;
     }
 
-    // Destroy the main buffer
-    B_DeleteBuffer(buffer);
+    auto win = sdl2::make_window("Hello World!", 100, 100, 320, 240, SDL_WINDOW_SHOWN);
+    if (!win) {
+        cerr << "Error creating window: " << SDL_GetError() << endl;
+        return 1;
+    }
 
-    shutdown();
+    auto ren
+        = sdl2::make_renderer(win.get(), -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (!ren) {
+        cerr << "Error creating renderer: " << SDL_GetError() << endl;
+        return 1;
+    }
+
+	const unsigned int WIDTH = 320;
+	const unsigned int HEIGHT = 240;
+	SDL_Texture* drawTex = NULL;
+	drawTex = SDL_CreateTexture(ren.get(), SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, WIDTH, HEIGHT);
+	SDL_SetTextureBlendMode(drawTex, SDL_BLENDMODE_BLEND);
+
+	SDL_Color nightSky = {20,0,20,255};
+	SDL_Color nightHorizon = {50,20,50,255};
+	SDL_Color eveningSky =  {0x5c,0x57,0xff,255};
+	SDL_Color eveningHorizon = {0xff,0x40,0x00,255};
+	SDL_Color white = {0xff,0xff,0xff,0xff};
+
+	// Generate background texture
+	uint32_t* texPixels = (uint32_t*)calloc(sizeof(uint32_t), WIDTH * HEIGHT);
+	PixBuffer background;
+	background.pixels = texPixels;
+	background.width = WIDTH;
+	background.height = HEIGHT;
+	SDL_Color colorBottom1 = {50,50,80,255};
+	PixBuffer_fillBuffer(&background, 0x00FFFF, 1.0);
+	drawTex = SDL_CreateTexture(ren.get(), SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, WIDTH, HEIGHT);
+	SDL_SetTextureBlendMode(drawTex, SDL_BLENDMODE_NONE);
+
+    for (int i = 0; i < 20; i++) {
+        SDL_RenderClear(ren.get());
+		SDL_UpdateTexture(drawTex, NULL, background.pixels, sizeof(uint32_t) * WIDTH);
+        SDL_RenderCopy(ren.get(), drawTex, nullptr, nullptr);
+        SDL_RenderPresent(ren.get());
+        SDL_Delay(100);
+    }
 
     return 0;
-}
-
-// Draw the maze stored in array maze[] into video
-// memory at address screen.
-void draw_maze(Buffer* buffer)
-{
-	struct xy block, lblock, rblock;
-	int oldleft, oldright;
-
-	// Draw the maze at each distance allowed by visibility
-	for(int dist = 0; dist < visibility; dist++)
-	{
-		// Find current square of maze
-		block.x = pos.x + dist * increment[direction].x;
-		block.y = pos.y + dist * increment[direction].y;
-
-		// Find square to the left of current square
-		lblock.x = block.x + left[direction].x;
-		lblock.y = block.y + left[direction].y;
-
-		// Find square to the right of current square
-		rblock.x = block.x + right[direction].x;
-		rblock.y = block.y + right[direction].y;
-
-		// Draw image of squares according to distance
-		switch(dist)
-		{
-			case 0:		// Draw current square
-
-				// Is wall open to the left?
-				// If not, draw wall
-				if (maze[block.x][block.y])
-				{
-					D_DrawLine(buffer, 82, 19, 135, 44, Color::GREY);
-					D_DrawLine(buffer, 135, 44, 135, 93, Color::GREY);
-					D_DrawLine(buffer, 135, 93, 82, 118, Color::GREY);
-				}
-				else // Else draw opening
-				{
-					D_DrawLine(buffer, 82, 44, 135, 44, Color::GREY);
-					D_DrawLine(buffer, 135, 44, 135, 93, Color::GREY);
-					D_DrawLine(buffer, 135, 93, 82, 93, Color::GREY);
-				}
-
-				// Is wall open to the right?
-				// If not, draw wall
-				if (maze[rblock.x][rblock.y])
-				{
-					D_DrawLine(buffer, 294, 19, 242, 44, Color::GREY);
-					D_DrawLine(buffer, 242, 44, 242, 93, Color::GREY);
-					D_DrawLine(buffer, 294, 118, 242, 93, Color::GREY);
-				}
-				else	// Else draw opening
-				{
-					D_DrawLine(buffer, 294, 44, 242, 44, Color::GREY);
-					D_DrawLine(buffer, 242, 44, 242, 93, Color::GREY);
-					D_DrawLine(buffer, 242, 93, 294, 93, Color::GREY);
-				}
-				break;
-
-			case 1:		// Repeat for next square
-
-				// Can we see the next square?
-				// If not, draw wall
-				if(maze[block.x][block.y])
-				{
-					D_DrawLine(buffer, 135, 44, 135, 93, Color::GREY);
-					D_DrawLine(buffer, 242, 44, 242, 93, Color::GREY);
-					D_DrawLine(buffer, 135, 44, 242, 44, Color::GREY);
-					D_DrawLine(buffer, 135, 93, 242, 93, Color::GREY);
-				}
-				else	// Else draw sides of the next square
-				{
-					if (maze[lblock.x][lblock.y])
-					{
-						D_DrawLine(buffer, 135, 44, 162, 57, Color::GREY);
-						D_DrawLine(buffer, 162, 57, 162, 80, Color::GREY);
-						D_DrawLine(buffer, 162, 80, 135, 93, Color::GREY);
-					}
-					else
-					{
-						D_DrawLine(buffer, 135, 57, 162, 57, Color::GREY);
-						D_DrawLine(buffer, 162, 57, 162, 80, Color::GREY);
-						D_DrawLine(buffer, 162, 80, 135, 80, Color::GREY);
-					}
-					if (maze[rblock.x][rblock.y])
-					{
-						D_DrawLine(buffer, 242, 44, 215, 57, Color::GREY);
-						D_DrawLine(buffer, 215, 57, 215, 80, Color::GREY);
-						D_DrawLine(buffer, 215, 80, 242, 93, Color::GREY);
-					}
-					else
-					{
-						D_DrawLine(buffer, 242, 57, 215, 57, Color::GREY);
-						D_DrawLine(buffer, 215, 57, 215, 80, Color::GREY);
-						D_DrawLine(buffer, 215, 80, 242, 80, Color::GREY);
-					}
-				}
-				break;
-
-			case 2:		// Do it again
-				if (maze[block.x][block.y])
-				{
-					D_DrawLine(buffer, 162, 57, 162, 80, Color::GREY);
-					D_DrawLine(buffer, 215, 57, 215, 80, Color::GREY);
-					D_DrawLine(buffer, 162, 57, 215, 57, Color::GREY);
-					D_DrawLine(buffer, 162, 80, 215, 80, Color::GREY);
-				}
-				else
-				{
-					if (maze[lblock.x][lblock.y])
-					{
-						D_DrawLine(buffer, 162, 57, 175, 63, Color::GREY);
-						D_DrawLine(buffer, 175, 63, 175, 74, Color::GREY);
-						D_DrawLine(buffer, 175, 74, 162, 80, Color::GREY);
-					}
-					else
-					{
-						D_DrawLine(buffer, 162, 63, 175, 63, Color::GREY);
-						D_DrawLine(buffer, 175, 63, 175, 74, Color::GREY);
-						D_DrawLine(buffer, 175, 74, 162, 74, Color::GREY);
-					}
-					if (maze[rblock.x][rblock.y])
-					{
-						D_DrawLine(buffer, 215, 57, 202, 63, Color::GREY);
-						D_DrawLine(buffer, 202, 63, 202, 74, Color::GREY);
-						D_DrawLine(buffer, 202, 74, 215, 80, Color::GREY);
-					}
-					else
-					{
-						D_DrawLine(buffer, 215, 63, 202, 63, Color::GREY);
-						D_DrawLine(buffer, 202, 63, 202, 74, Color::GREY);
-						D_DrawLine(buffer, 202, 74, 215, 74, Color::GREY);
-					}
-				}
-				break;
-
-			case 3:		// And again
-				if (maze[block.x][block.y])
-				{
-					D_DrawLine(buffer, 175, 63, 175, 74, Color::GREY);
-					D_DrawLine(buffer, 202, 63, 202, 74, Color::GREY);
-					D_DrawLine(buffer, 175, 63, 202, 63, Color::GREY);
-					D_DrawLine(buffer, 175, 74, 202, 74, Color::GREY);
-				}
-				else
-				{
-					if (maze[lblock.x][lblock.y])
-					{
-						D_DrawLine(buffer, 175, 63, 182, 66, Color::GREY);
-						D_DrawLine(buffer, 182, 66, 182, 70, Color::GREY);
-						D_DrawLine(buffer, 182, 70, 175, 74, Color::GREY);
-					}
-					else
-					{
-						D_DrawLine(buffer, 175, 66, 182, 66, Color::GREY);
-						D_DrawLine(buffer, 182, 66, 182, 70, Color::GREY);
-						D_DrawLine(buffer, 182, 70, 175, 70, Color::GREY);
-					}
-					if (maze[rblock.x][rblock.y])
-					{
-						D_DrawLine(buffer, 202, 63, 195, 66, Color::GREY);
-						D_DrawLine(buffer, 195, 66, 195, 70, Color::GREY);
-						D_DrawLine(buffer, 195, 70, 202, 74, Color::GREY);
-					}
-					else
-					{
-						D_DrawLine(buffer, 202, 66, 195, 66, Color::GREY);
-						D_DrawLine(buffer, 195, 66, 195, 70, Color::GREY);
-						D_DrawLine(buffer, 195, 70, 202, 70, Color::GREY);
-					}
-				}
-				break;
-		}
-
-		// If view is obscured by wall, stop drawing
-		if(maze[block.x][block.y]) break;
-	}
-}
-
-void draw_box(Buffer* buffer)
-{
-	//left, top, right, bottom
-	//nasl_draw_rect(buffer, 82, 19, 294, 119, Color::GREY);
-}
-
-static int init(int width, int height)
-{
-    S_Init("Neogardens Wireframe Maze Demo", width, height);
-
-    //glfwSetKeyCallback(nasl_graphics_get_window(), key_callback);
-
-    return 1;
-}
-
-//static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-//{
-//    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-//        glfwSetWindowShouldClose(window, GLFW_TRUE);
-//}
-
-static int shutdown()
-{
-    S_Quit();
-
-    return 1;
 }
