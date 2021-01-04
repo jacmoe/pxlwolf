26942f665c33987b3906a2a15ce5d7dae95566c3
Jacob Moen <jacmoe.dk@gmail.com>
2021-01-05 00:02:06 +0100

    Now we can load different levels

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -50,7 +50,7 @@ double deg2rad (double degrees) {
     return degrees * 4.0 * atan (1.0) / 180.0;
 }
 
-bool load_level(const std::string level_name, bool from_zip = false)
+bool load_level(const std::string level_file, const std::string level_name, bool from_zip = false)
 {
     char readBuffer[65536];
     rapidjson::Document document;
@@ -58,11 +58,11 @@ bool load_level(const std::string level_name, bool from_zip = false)
 
     if(from_zip)
 	{
-		PhysFS::ifstream fp(level_name);
+		PhysFS::ifstream fp(level_file);
 		rapidjson::IStreamWrapper isw(fp);
 	    document.ParseStream(isw);
 	} else {
-		fp = fopen(level_name.c_str(), "rb");
+		fp = fopen(level_file.c_str(), "rb");
     	rapidjson::FileReadStream is(fp, readBuffer, sizeof(readBuffer));
 	    document.ParseStream(is);
 	}
@@ -70,42 +70,46 @@ bool load_level(const std::string level_name, bool from_zip = false)
     const rapidjson::Value& levels = document["levels"];
     for (rapidjson::Value::ConstValueIterator itr = levels.Begin(); itr != levels.End(); ++itr)
     {
-        const rapidjson::Value& layerInstances = (*itr)["layerInstances"];
-        for (rapidjson::Value::ConstValueIterator itr = layerInstances.Begin(); itr != layerInstances.End(); ++itr)
-        {
-            std::string layer_type = (*itr)["__type"].GetString();
-            if(layer_type == "Entities")
-            {
-                const rapidjson::Value& entityInstances = (*itr)["entityInstances"];
-                for (rapidjson::Value::ConstValueIterator itr = entityInstances.Begin(); itr != entityInstances.End(); ++itr)
-                {
-                    std::string identifier = (*itr)["__identifier"].GetString();
-                    if(identifier == "PlayerStart")
-                    {
-                        player_x = (*itr)["__grid"].GetArray()[0].GetFloat() + 0.5f;
-                        player_y = (*itr)["__grid"].GetArray()[1].GetFloat() + 0.5f;
-                        player_heading = static_cast<float>(deg2rad((*itr)["fieldInstances"].GetArray()[0]["__value"].GetFloat()));
-                    }
-                }
-            }
-            if(layer_type == "IntGrid")
-            {
-                map_width = (*itr)["__cWid"].GetInt();
-                map_height = (*itr)["__cHei"].GetInt();
+        std::string level_name_ = (*itr)["identifier"].GetString();
+		if(level_name_ == level_name)
+		{
+			const rapidjson::Value& layerInstances = (*itr)["layerInstances"];
+			for (rapidjson::Value::ConstValueIterator itr = layerInstances.Begin(); itr != layerInstances.End(); ++itr)
+			{
+				std::string layer_type = (*itr)["__type"].GetString();
+				if(layer_type == "Entities")
+				{
+					const rapidjson::Value& entityInstances = (*itr)["entityInstances"];
+					for (rapidjson::Value::ConstValueIterator itr = entityInstances.Begin(); itr != entityInstances.End(); ++itr)
+					{
+						std::string identifier = (*itr)["__identifier"].GetString();
+						if(identifier == "PlayerStart")
+						{
+							player_x = (*itr)["__grid"].GetArray()[0].GetFloat() + 0.5f;
+							player_y = (*itr)["__grid"].GetArray()[1].GetFloat() + 0.5f;
+							player_heading = static_cast<float>(deg2rad((*itr)["fieldInstances"].GetArray()[0]["__value"].GetFloat()));
+						}
+					}
+				}
+				if(layer_type == "IntGrid")
+				{
+					map_width = (*itr)["__cWid"].GetInt();
+					map_height = (*itr)["__cHei"].GetInt();
 
-				// set the size of levelMap and fill it with zeroes.
-				levelMap.assign(map_width * map_height, 0);
+					// set the size of levelMap and fill it with zeroes.
+					levelMap.assign(map_width * map_height, 0);
 
-                const rapidjson::Value& initGrid = (*itr)["intGrid"];
+					const rapidjson::Value& initGrid = (*itr)["intGrid"];
 
-                for (rapidjson::Value::ConstValueIterator itr = initGrid.Begin(); itr != initGrid.End(); ++itr)
-                {
-					// Add one to wall values so that zero becomes walkable area
-					levelMap[(*itr)["coordId"].GetInt()] = (*itr)["v"].GetInt() + 1;
-                }
-            }
+					for (rapidjson::Value::ConstValueIterator itr = initGrid.Begin(); itr != initGrid.End(); ++itr)
+					{
+						// Add one to wall values so that zero becomes walkable area
+						levelMap[(*itr)["coordId"].GetInt()] = (*itr)["v"].GetInt() + 1;
+					}
+				}
 
-        }
+			}
+		}
     }
 	if(from_zip)
 	{
@@ -329,7 +333,7 @@ int main(int, char**)
 	//PhysFS::init (nullptr);
 	//PhysFS::mount("assets.zip", "", 1);
 
-    load_level("assets/levels/level.ldtk");
+    load_level("assets/levels/levels.ldtk", "Level1");
 
 	SDL_Init(SDL_INIT_VIDEO);
 
