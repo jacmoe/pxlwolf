035e67315f698c7f8626a8919b294e3a8d4e8e1d
Jacob Moen <jacmoe.dk@gmail.com>
2021-01-12 21:00:13 +0100

    Removing palette duplicates

--- code/src/Game.cpp
+++ code/src/Game.cpp
@@ -18,12 +18,42 @@
 #include "lua_main.hpp"
 #include "Map.hpp"
 #include "physfs.hpp"
+#include <set>
 
 Game::Game()
 {}
 
 Game::~Game()
 {
+}
+void removeDuplicates(std::vector<sf::Color>& arr) {
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = i + 1; j < arr.size(); j++)
+        {
+            if (arr[i].toInteger() > arr[j].toInteger())
+            {
+                sf::Color temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    std::vector<sf::Color> y;
+    sf::Color x = arr[0];
+    int i = 0;
+    while (i < arr.size())
+    {
+        if (x != arr[i])
+        {
+            y.push_back(x);
+            x = arr[i];
+        }
+        i++;
+        if (i == arr.size())
+            y.push_back(arr[i - 1]);
+    }
+    arr = y;
 }
 
 int getIndex(std::vector<sf::Color> v, sf::Color color)
@@ -73,22 +103,38 @@ bool Game::OnUserCreate()
     image.create(atlas.GetImageDimensions().x, atlas.GetImageDimensions().y, atlas.GetPixels(7));
 
     std::vector<sf::Color> the_palette;
-    for(unsigned int px = 0; px < image.getSize().x; px++ )
-    {
-        for(unsigned int py = 0; py < image.getSize().y; py++ )
-        {
-            the_palette.push_back(image.getPixel(px, py));
-        }
-    }
-
-    m_pixeldisplay.setPalette(the_palette);
-
     int idx = 0;
     for(unsigned int px = 0; px < image.getSize().x; px++ )
     {
         for(unsigned int py = 0; py < image.getSize().y; py++ )
         {
-            m_pixeldisplay.setPixel(m_pixeldisplay.getIndex(sf::Vector2u(px, py)), idx);
+            the_palette.push_back(image.getPixel(px, py));
+            idx++;
+        }
+    }
+
+    SPDLOG_INFO("Size of the_palette before duplicate removal: {}", the_palette.size());
+    removeDuplicates(the_palette);
+    SPDLOG_INFO("Size of the_palette after duplicate removal: {}", the_palette.size());
+
+    std::vector<unsigned int> the_colors;
+    for(unsigned int px = 0; px < image.getSize().x; px++ )
+    {
+        for(unsigned int py = 0; py < image.getSize().y; py++ )
+        {
+            the_colors.push_back(getIndex(the_palette, image.getPixel(px, py)));
+        }
+    }
+
+
+    m_pixeldisplay.setPalette(the_palette);
+
+    idx = 0;
+    for(unsigned int px = 0; px < image.getSize().x; px++ )
+    {
+        for(unsigned int py = 0; py < image.getSize().y; py++ )
+        {
+            m_pixeldisplay.setPixel(m_pixeldisplay.getIndex(sf::Vector2u(px, py)), the_colors[idx]);
             idx++;
         }
     }
