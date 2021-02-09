e1f7396b5b04fe9dd19f89347effd2e3199cf50e
Jacob Moen <jacmoe.dk@gmail.com>
2021-02-10 00:06:00 +0100

    Level 4 wall decorated

--- code/main/Game.cpp
+++ code/main/Game.cpp
@@ -123,7 +123,7 @@ bool Game::OnUserCreate()
 {
     utility::Map* map = m_map.get();
     map->init("assets/levels/pxlwolf.ldtk");
-    map->load("Level3");
+    map->load("Level4");
 
     write_text("PixelWolf");
 
