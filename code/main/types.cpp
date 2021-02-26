646e1515223dd84b4f5632feca5e2a02ffb2c13d
Jacob Moen <jacmoe.dk@gmail.com>
2021-02-26 15:18:14 +0100

    Common types put in own file

---
+++ code/main/types.cpp
@@ -0,0 +1,42 @@
+﻿/*# This file is part of the
+# ██████╗ ██╗  ██╗██╗     ██╗    ██╗ ██████╗ ██╗     ███████╗
+# ██╔══██╗╚██╗██╔╝██║     ██║    ██║██╔═══██╗██║     ██╔════╝
+# ██████╔╝ ╚███╔╝ ██║     ██║ █╗ ██║██║   ██║██║     █████╗
+# ██╔═══╝  ██╔██╗ ██║     ██║███╗██║██║   ██║██║     ██╔══╝
+# ██║     ██╔╝ ██╗███████╗╚███╔███╔╝╚██████╔╝███████╗██║
+# ╚═╝     ╚═╝  ╚═╝╚══════╝ ╚══╝╚══╝  ╚═════╝ ╚══════╝╚═╝
+#   project
+#
+#   https://github.com/jacmoe/pxlwolf
+#
+#   (c) 2020 - 2021 Jacob Moena
+#
+#   MIT License
+#*/
+#include "main/types.hpp"
+
+Vector2i::Vector2i(int x, int y)
+    : x(x)
+    , y(y)
+{
+}
+
+Vector2f::Vector2f(float x, float y)
+    : x(x)
+    , y(y)
+{
+}
+
+Vector2d::Vector2d(double x, double y)
+    : x(x)
+    , y(y)
+{
+}
+
+IntRect::IntRect(int left, int top, int width, int height)
+    : left(left)
+    , top(top)
+    , width(width)
+    , height(height)
+{
+}
---
+++ code/main/types.hpp
@@ -0,0 +1,54 @@
+﻿/*# This file is part of the
+# ██████╗ ██╗  ██╗██╗     ██╗    ██╗ ██████╗ ██╗     ███████╗
+# ██╔══██╗╚██╗██╔╝██║     ██║    ██║██╔═══██╗██║     ██╔════╝
+# ██████╔╝ ╚███╔╝ ██║     ██║ █╗ ██║██║   ██║██║     █████╗
+# ██╔═══╝  ██╔██╗ ██║     ██║███╗██║██║   ██║██║     ██╔══╝
+# ██║     ██╔╝ ██╗███████╗╚███╔███╔╝╚██████╔╝███████╗██║
+# ╚═╝     ╚═╝  ╚═╝╚══════╝ ╚══╝╚══╝  ╚═════╝ ╚══════╝╚═╝
+#   project
+#
+#   https://github.com/jacmoe/pxlwolf
+#
+#   (c) 2020 - 2021 Jacob Moena
+#
+#   MIT License
+#*/
+#pragma once
+
+struct Vector2i
+{
+    int x;
+    int y;
+
+    Vector2i() = default;
+    Vector2i(int x, int y);
+};
+
+struct Vector2f
+{
+    float x;
+    float y;
+
+    Vector2f() = default;
+    Vector2f(float x, float y);
+};
+
+struct Vector2d
+{
+    float x;
+    float y;
+
+    Vector2d() = default;
+    Vector2d(double x, double y);
+};
+
+struct IntRect
+{
+    int left;
+    int top;
+    int width;
+    int height;
+
+    IntRect() = default;
+    IntRect(int left, int top, int width, int height);
+};
